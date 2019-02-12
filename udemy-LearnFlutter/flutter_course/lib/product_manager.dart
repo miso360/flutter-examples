1f0aa7dfb448e8961734e947b4b5ad8dafbbb53d
Jun Park <dev.miso360@gmail.com>
2019-02-12 23:49:34 +0900

    오늘도 코딩중

--- udemy-LearnFlutter/flutter_course/lib/product_manager.dart
+++ udemy-LearnFlutter/flutter_course/lib/product_manager.dart
@@ -16,7 +16,7 @@ class ProductManager extends StatefulWidget {
 }
 
 class _ProductManager extends State<ProductManager> {
-  List<String> _products = [];
+  final List<String> _products = [];
 
   @override
   void initState() {
