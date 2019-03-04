ba00f60eae1bfc6d29ec3666982451aea0d84465
Jun Park <dev.miso360@gmail.com>
2019-03-04 23:56:32 +0900

    Decoration

--- udemy-LearnFlutter/flutter_course/lib/products.dart
+++ udemy-LearnFlutter/flutter_course/lib/products.dart
@@ -24,9 +24,20 @@ class Products extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                         fontFamily: 'Oswald'),
                   ),
-                  SizedBox(width: 8.0,),
-                  Text(
-                    products[index]['price'].toString(),
+                  SizedBox(
+                    width: 8.0,
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).accentColor,
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    child: Text(
+                      '\$${products[index]['price'].toString()}',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   )
                 ],
               )),
