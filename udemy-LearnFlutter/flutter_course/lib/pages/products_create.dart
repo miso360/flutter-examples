3e77d2878fb5730dc36c3bdd0b64c231702cbb75
Jun Park <dev.miso360@gmail.com>
2019-03-31 18:34:06 +0900

    Validation

--- udemy-LearnFlutter/flutter_course/lib/pages/products_create.dart
+++ udemy-LearnFlutter/flutter_course/lib/pages/products_create.dart
@@ -19,40 +19,56 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
 
   Widget _buildTitleTextField() {
     return TextFormField(
-      decoration: InputDecoration(labelText: 'Product Title'),
-      onSaved: (String value) {
-        setState(() {
-          _titleValue = value;
+        decoration: InputDecoration(labelText: 'Product Title'),
+        validator: (String value) {
+          if (value.isEmpty || value.length < 5) {
+            return 'Title is required and should be 5+ characters long.';
+          }
+        },
+        onSaved: (String value) {
+          setState(() {
+            _titleValue = value;
+          });
         });
-      }
-    );
   }
 
   Widget _buildDescriptionTextField() {
     return TextFormField(
-      maxLines: 4,
-      decoration: InputDecoration(labelText: 'Product Description'),
+        maxLines: 4,
+        decoration: InputDecoration(labelText: 'Product Description'),
+        validator: (String value) {
+          if (value.isEmpty || value.length < 10) {
+            return 'Description is required and should be 10+ characters long.';
+          }
+        },
         onSaved: (String value) {
           setState(() {
             _descriptionValue = value;
           });
-        }
-    );
+        });
   }
 
   Widget _buildPriceTextField() {
     return TextFormField(
-      keyboardType: TextInputType.number,
-      decoration: InputDecoration(labelText: 'Product Price'),
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(labelText: 'Product Price'),
+        validator: (String value) {
+          if (value.isEmpty ||
+              !RegExp(r'^(?:[1-9]\d*|0)?(?:[.,]\d+)?$').hasMatch(value)) {
+            return 'Price is required and should be a number.';
+          }
+        },
         onSaved: (String value) {
           setState(() {
             _priceValue = double.parse(value);
           });
-        }
-    );
+        });
   }
 
   void _submitForm() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
     _formKey.currentState.save();
     final Map<String, dynamic> product = {
       'title': _titleValue,
@@ -70,24 +86,28 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     final double targetWidth = deviceWidth > 500.0 ? 500.0 : deviceWidth * 0.95;
     final double targetPadding = deviceWidth - targetWidth;
 
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
-          children: <Widget>[
-            _buildTitleTextField(),
-            _buildDescriptionTextField(),
-            _buildPriceTextField(),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-              child: Text('Save'),
-              textColor: Colors.white,
-              onPressed: _submitForm,
-            )
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
+            children: <Widget>[
+              _buildTitleTextField(),
+              _buildDescriptionTextField(),
+              _buildPriceTextField(),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                child: Text('Save'),
+                textColor: Colors.white,
+                onPressed: _submitForm,
+              )
 //          GestureDetector(
 //            onTap: _submitForm,
 //            child: Container(
@@ -96,7 +116,8 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
 //              child: Text('My Button'),
 //            ),
 //          )
-          ],
+            ],
+          ),
         ),
       ),
     );
