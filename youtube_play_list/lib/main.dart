55e0248232d28bf756f54769f46cfb81317a30f8
Jun Park <dev.miso360@gmail.com>
2019-09-08 14:29:57 +0900

    Start YouTube play list project

---
+++ youtube_play_list/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'YouTube play list',
+      home: new Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: new AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      body: new Container(
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+              image: new AssetImage("img/bg.jpg"), fit: BoxFit.cover),
+        ),
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new SizedBox(
+                width: 150.0,
+                height: 150.0,
+                child: new Image.asset("img/ic.png"),
+              ),
+              new Text(
+                "YouTube play list",
+                style: new TextStyle(
+                  fontSize: 20,
+                  color: Colors.white
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
